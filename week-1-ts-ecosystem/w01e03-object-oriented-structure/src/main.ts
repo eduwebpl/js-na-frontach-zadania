46c7beb1587a105c1e55f4d44bdd7841bf91aa39
DragoBJJ <j.pawelski.it@gmail.com>
2022-09-24 20:24:54 +0200

    Create logic to Shopping Card class nad create types

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
@@ -1,11 +1,37 @@
-/**
- * To tutaj mamy "START" programu.
- *
- * Przygotuj tutaj kawałek kodu potwierdzający poprawność działania koszyka.
- *
- * Np. Utwórz 3 różne koszyki — dodaj do nich różne produkty.
- * Potem wyświetl te produkty.
- * Wykaż, że koszyki mają różne produkty — inną ich ilość etc.
- * Przygotuj koszyki dla każdego rodzaju produktów.
- * - po prostu: wykaż, że przygotowana logika i modele danych — działają :)
- * */
+import {ProductCart, ProductType, UpdateProductData, vinylRecord} from "./type";
+
+
+class ShoppingCart<Type extends ProductType> {
+    #products: ProductCart<Type>[] = []
+
+        getProducts(): ProductCart<Type>[]  {
+         return this.#products
+     }
+
+     getProduct(id: number): ProductCart<Type> |  undefined {
+         return  this.#products.find(p => p.id === id)
+     }
+      addProduct(product: ProductCart<Type>): void {
+        this.#products.push(product)
+      }
+      updateProduct(productID: ProductCart<Type>["id"], productData: UpdateProductData<Type>): void {
+          this.#products = this.#products.map(product => {
+            if (product.id === productID) {
+                return {
+                    ...product,
+                    ...productData
+                }
+            }
+            return product;
+        })
+      }
+}
+
+const  VinylShoppingCart = new ShoppingCart<ProductType.BUY_NOW>()
+    VinylShoppingCart.addProduct((vinylRecord))
+const myAllProducts = VinylShoppingCart.getProducts()
+console.log("myAllProducts", myAllProducts)
+
+VinylShoppingCart.updateProduct(1,{name:"Elegancki update", amount: 5000, price: 500})
+const updatedProduct = VinylShoppingCart.getProduct(1)
+console.log("updatedProduct", updatedProduct)

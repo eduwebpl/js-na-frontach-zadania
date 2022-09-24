cc43177a9fb6c6096f6e69c5dfa3c715109cb67f
DragoBJJ <j.pawelski.it@gmail.com>
2022-09-24 21:06:58 +0200

    Add protected and public behavior

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/main.ts
@@ -4,17 +4,22 @@ import {ProductCart, ProductType, UpdateProductData, vinylRecord} from "./type";
 class ShoppingCart<Type extends ProductType> {
     #products: ProductCart<Type>[] = []
 
-        getProducts(): ProductCart<Type>[]  {
+
+    public getProducts(): ProductCart<Type>[]  {
          return this.#products
      }
-
-     getProduct(id: number): ProductCart<Type> |  undefined {
+    public getLengthAllProducts(): number {
+        const allProducts  = this.getProducts()
+          if(allProducts.length) return allProducts.length
+         return 0
+     }
+        public getProduct(id: number): ProductCart<Type> |  undefined {
          return  this.#products.find(p => p.id === id)
      }
-      addProduct(product: ProductCart<Type>): void {
+      protected  addProduct(product: ProductCart<Type>): void {
         this.#products.push(product)
       }
-      updateProduct(productID: ProductCart<Type>["id"], productData: UpdateProductData<Type>): void {
+    protected  updateProduct(productID: ProductCart<Type>["id"], productData: UpdateProductData<Type>): void {
           this.#products = this.#products.map(product => {
             if (product.id === productID) {
                 return {
@@ -25,13 +30,24 @@ class ShoppingCart<Type extends ProductType> {
             return product;
         })
       }
+    protected deleteProduct(id: ProductCart<Type>["id"]): void {
+    this.#products = this.#products.filter(p => p.id!== id)}
 }
 
+
+ let myAllProducts;
 const  VinylShoppingCart = new ShoppingCart<ProductType.BUY_NOW>()
-    VinylShoppingCart.addProduct((vinylRecord))
-const myAllProducts = VinylShoppingCart.getProducts()
+    // VinylShoppingCart.addProduct((vinylRecord))
+ myAllProducts = VinylShoppingCart.getProducts()
 console.log("myAllProducts", myAllProducts)
 
-VinylShoppingCart.updateProduct(1,{name:"Elegancki update", amount: 5000, price: 500})
+// VinylShoppingCart.updateProduct(1,{name:"Elegancki update", amount: 5000, price: 500})
 const updatedProduct = VinylShoppingCart.getProduct(1)
 console.log("updatedProduct", updatedProduct)
+
+// VinylShoppingCart.deleteProduct(1)
+ myAllProducts = VinylShoppingCart.getProducts()
+console.log("myAllProducts", myAllProducts)
+
+const LengthAllProducts = VinylShoppingCart.getLengthAllProducts()
+console.log("LengthAllProducts", LengthAllProducts)

82df082cafff6d42e9964b480cddd8ce0b17a99b
Krzysztof Branecki <krzysztof.branecki@frontkom.com>
2022-10-21 19:03:45 +0200

    solution/week1/e01 Add types and dividing into modules

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
@@ -5,8 +5,9 @@
  * całość programu musi jednak działać tak jak do tej pory !
  *
  * */
+import { hasGivenAge, hasAddress } from './utils'
 
-const user: any = {
+const user = {
   name: 'Andy',
   age: 30,
   email: 'andy@mail-me-tommorow.com',
@@ -14,14 +15,6 @@ const user: any = {
     street: 'Strange Alley',
     no: 23,
   },
-}
-
-function hasAddress(user: any): any {
-  return Boolean(user.address)
-}
-
-function hasGivenAge(requiredAge: any): any {
-  return (user: any): any => user.age >= requiredAge
 }
 
 const isAdult = hasGivenAge(18)
